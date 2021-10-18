77c8cbc682acdecff57487d593268a6398f637a4
Peter Conley <peterdavidconley@gmail.com>
2021-10-18 13:58:19 -0400

    created homepage.css file and four sections in JSX.

---
+++ african-marketplace/src/components/Homepage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Homepage () {
+    return(
+        <div>
+            <section id='description'></section>
+                {/* Selina's code here*/}
+            <section id='about-us'>
+                {/* Selina's code here*/}
+            </section>
+            <section id='squares'>
+                
+            </section>
+            <section id='footer'>
+
+            </section>
+        </div>
+    )
+}
+
+export default Homepage
