86f5d32b0e697a4638e46ff6bc6e43904881ea0f
Peter Conley <peterdavidconley@gmail.com>
2021-10-18 14:31:36 -0400

    added styling and base HTML elements

--- african-marketplace/src/components/Homepage.js
+++ african-marketplace/src/components/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import '../Homepage.css'
 
 function Homepage () {
     return(
@@ -10,10 +11,15 @@ function Homepage () {
                 {/* Selina's code here*/}
             </section>
             <section id='squares'>
-                
+                <div id='explore'><p>Explore Listings</p></div>
+                <div id='seller'><p>Become a Seller</p></div>
+                <div id='business'><p>Explore Business</p></div>
             </section>
             <section id='footer'>
-
+                <h2>Contact Us</h2>
+                <br />
+                <p>Email: hello@africanmarketplace.com</p>
+                <p>Phone: 716-555-1234</p>
             </section>
         </div>
     )
