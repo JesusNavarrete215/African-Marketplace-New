85b7f86fb7fde799af41b8e47e044e6f28680b98
jcpcabanada <jcpcabanada@gmail.com>
2021-10-18 16:42:15 -0700

    set routes to correct files and added a logo.png to components folder and imported to app.js

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -2,17 +2,58 @@ import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import Homepage from './components/Homepage'
+import { Link, Route, Switch } from 'react-router-dom';
 
+//components for nav
+import Homepage from './components/Homepage';
+import CreateAccount from './components/CreateAccount';
+import Itemlist from "./components/Itemlist";
+import AddItem from "./components/AddItem"
 import Login from "./components/Login"; //login form
 import Itemlist from "./components/Itemlist"; //all the items for sale will be located in
+import AfricaLogo from './components/AfricaLogo.png';
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <Login />
-      <Itemlist />
-      <Homepage />
+      <div className='market-header'>
+        <img src={AfricaLogo} alt='green Africa logo' />
+        <nav>
+          <h1 className='TitleTop'>African Marketplace</h1>
+          <div className='nav-links'>
+            <Link to="/">Home</Link>
+            <Link to="/create-an-account">Create An Account</Link>
+            <Link to="/marketplace">MarketPlace</Link>
+            <Link to="/create-listing">Create Listings</Link>
+            <Link to="/login">login</Link>
+          </div>
+        </nav>
+      </div>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/create-listing">
+          <AddItem />
+        </Route>
+
+        <Route path="/marketplace">
+          <Itemlist />
+        </Route>
+
+        <Route path="/create-an-account">
+          <CreateAccount />
+        </Route>
+
+        <Route path='/'>
+          <Homepage />
+        </Route>
+      </Switch>
     </div>
   );
 }
