7d58c83d8e405e2166ee3e723d6d14ddab64871b
jcpcabanada <jcpcabanada@gmail.com>
2021-10-21 19:38:17 -0700

    added some code of switch and link for navigation and a .png for logo

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link, Route, Switch } from 'react-router-dom';
 import Homepage from "./components/Homepage";
 import Login from "./components/Login"; //login form
 import Itemlist from "./components/Itemlist"; //all the items for sale will be located in
@@ -8,26 +8,44 @@ import Itemlist from "./components/Itemlist"; //all the items for sale will be l
 import PrivateRoute from "./components/privateRoute";
 import AddItem from "./components/AddItem";
 import SignUp from "./components/SignUp";
+import AfricaLogo from './components/AfricaLogo.png';
 import { connect } from "react-redux";
 
 function App() {
   return (
-    <div>
-      <Route exact path="/">
-        <Homepage />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/add-item">
-        <AddItem />
-      </Route>
-      <Route path="/signup">
-        <SignUp />
-      </Route>
-      {/* <PrivateRoute exact path="/item-list" component={Itemlist} /> */}
+    <div className="App">
+      <div className='market-header'>
+        <img src={AfricaLogo} alt='green Africa logo' />
+        <nav>
+          <h1 className='TitleTop'>African Marketplace</h1>
+          <div className='nav-links'>
+            <Link to="/">Home</Link>
+            <Link to="/login">login</Link>
+            <Link to="/add_item">Create Listings</Link>
+            <Link to="/signup">Sign Up</Link>
+
+          </div>
+        </nav>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/add-item">
+          <AddItem />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        {/* <PrivateRoute exact path="/item-list" component={Itemlist} /> */}
+        </Switch>
     </div>
   );
 }
 
 export default connect(null)(App);
+
+
